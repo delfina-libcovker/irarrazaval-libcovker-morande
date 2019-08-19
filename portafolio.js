0b0ee0c3de0d2debeeeca97982d1b49b1ab161da
delfina-libcovker <53878073+delfina-libcovker@users.noreply.github.com>
2019-08-19 11:28:15 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,38 +1,48 @@
 var trabajos = [{
     figure: "media/trabajo-01-desfile.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de primer trabajo",
+    title: "Pass",
     about: "Texto simulado de descripción de primer trabajo"
 }, {
     figure: "media/trabajo-02-app.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de segundo trabajo",
+    title: "Esquilarte",
     about: "Texto simulado de descripción de segundo trabajo"
 }, {
     figure: "media/trabajo-03-homy.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de tercer trabajo",
+    title: "PAD",
     about: "Texto simulado de descripción de tercer trabajo"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de cuarto trabajo",
+    title: "Kurve",
     about: "Texto simulado de descripción de cuarto trabajo"
 }, {
     figure: "media/trabajo-05-artesania.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de quinto trabajo",
+    title: "OXOFLEX",
     about: "Texto simulado de descripción de quinto trabajo"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
-    title: "Título de sexto trabajo",
+    title: "África",
     about: "Texto simulado de descripción de sexto trabajo"
 }, {
-    figure: "media/trabajo-07-materiales.jpg",
-    alt: "Descripción de la imagen",
-    title: "Título de séptimo trabajo",
-    about: "Texto simulado de descripción de séptimo trabajo"
+  figure: "media/trabajo-07-materiales.jpg",
+  alt: "Descripción de la imagen",
+  title: "Loconta.wer",
+  about: "Texto simulado de descripción de séptimo trabajo"
+}, {
+  figure: "media/trabajo-07-materiales.jpg",
+  alt: "Descripción de la imagen",
+  title: "Conalma",
+  about: "Texto simulado de descripción de séptimo trabajo"
+}, {
+  figure: "media/trabajo-07-materiales.jpg",
+  alt: "Descripción de la imagen",
+  title: "Alba",
+  about: "Texto simulado de descripción de séptimo trabajo"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
