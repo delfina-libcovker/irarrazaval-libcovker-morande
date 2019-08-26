59e979a4e95bb9db448936328d325e3b1d46d866
irarrazaval-libcovker-morande <53878073+delfina-libcovker@users.noreply.github.com>
2019-08-25 23:37:57 -0400

    Add files via upload

--- portafolio.js
+++ portafolio.js
@@ -1,53 +1,51 @@
 var trabajos = [{
     figure: "media/trabajo-01-desfile.jpg",
     alt: "Descripción de la imagen",
-    title: "Pass",
-    about: "Texto simulado de descripción de primer trabajo"
+    title: "Campaña free de nipple",
+    about: "Workshop liberación femenina",
+    url: "https://pousta.com/freethenipple/"
 }, {
     figure: "media/trabajo-02-app.jpg",
     alt: "Descripción de la imagen",
-    title: "Esquilarte",
-    about: "Texto simulado de descripción de segundo trabajo"
+    title: "Grita",
+    about: "Gráfica para ONG feminista",
+    url: "https://www.instagram.com/grita_grlpwr/"
 }, {
     figure: "media/trabajo-03-homy.jpg",
     alt: "Descripción de la imagen",
-    title: "PAD",
-    about: "Texto simulado de descripción de tercer trabajo"
+    title: "África",
+    about: "Eva",
+    url: "https://eacnur.org/es/actualidad/imagenes/mujeres-africanas-50-fotografias-retratan-la-vida-en-africa"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
-    alt: "Descripción de la imagen",
-    title: "Kurve",
-    about: "Texto simulado de descripción de cuarto trabajo"
+    alt: "",
+    title: "Revista Femme",
+    about: "Diseño de revista;textil como producto",
+    url: "http://revistafemmecolombia.com/"
 }, {
     figure: "media/trabajo-05-artesania.jpg",
     alt: "Descripción de la imagen",
-    title: "OXOFLEX",
-    about: "Texto simulado de descripción de quinto trabajo"
+    title: "Marcha",
+    about: "Producción fotográfica marcha 8 de marzo",
+    url: "https://www.youtube.com/watch?v=LabXibbtP2g"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
-    title: "África",
-    about: "Texto simulado de descripción de sexto trabajo"
+    title: "8M",
+    about: "Ilustración para el día de la mujer",
+    url: "https://redfeminismo.wordpress.com/2016/02/29/10-ilustradoras-feministas-que-no-debes-perderte/"
 }, {
-  figure: "media/trabajo-07-materiales.jpg",
-  alt: "Descripción de la imagen",
-  title: "Loconta.wer",
-  about: "Texto simulado de descripción de séptimo trabajo"
-}, {
-  figure: "media/trabajo-07-materiales.jpg",
-  alt: "Descripción de la imagen",
-  title: "Conalma",
-  about: "Texto simulado de descripción de séptimo trabajo"
-}, {
-  figure: "media/trabajo-07-materiales.jpg",
-  alt: "Descripción de la imagen",
-  title: "Alba",
-  about: "Texto simulado de descripción de séptimo trabajo"
+    figure: "media/trabajo-07-materiales.jpg",
+    alt: "Descripción de la imagen",
+    title: "Ponte color",
+    about: "Clases de auto maquillaje",
+    url: "https://www.instagram.com/maquillaje_aileighton/"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
     var uno = '<figure><img src="' + trabajos[i].figure + '" alt="' + trabajos[i].alt + '"/></figure>';
     var dos = '<h4>' + trabajos[i].title + '</h4>';
     var tres = '<p>' + trabajos[i].about + '</p>';
-    $('#portafolio').append('<div class="item">' + uno + dos + tres + '</div>')
+    var cuatro = '<p><a href= "' + trabajos[i].url + '">Ver más</a><p>'
+    $('#portafolio').append('<div class="item">' + uno + dos + tres + cuatro + '</div>')
 });
